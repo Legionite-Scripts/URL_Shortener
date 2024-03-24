67ec5a47009ed206e5085be1b4a33426530f04f1
legionite-scripts <samuelonwuka88@gmail.com>
2024-03-24 12:38:51 +0100

    Added loading animation

--- pages/Home.vue
+++ pages/Home.vue
@@ -10,7 +10,12 @@
       />
       <button type="submit" class="mb-2">Submit</button>
       <div v-if="shortLink">
-        <a :href="shortLink" ref="shortenedLink" target="_blank">{{ shortLink }}</a>
+        <a :href="shortLink" ref="shortenedLink" target="_blank">{{
+          shortLink
+        }}</a>
+      </div>
+      <div v-show="isLoading" class="loading-container">
+        <div class="loading-icon"></div>
       </div>
     </form>
   </div>
@@ -21,31 +26,41 @@ export default {
   data() {
     return {
       shortLink: '',
+      isLoading: false, // New data property to control loading animation
     }
   },
-
-  mounted() {
-    let form = this.$refs.urlShortenerForm //Identifying the form
-
-    form.addEventListener('submit', async (e) => { //If a submit action is applied on the form
-      e.preventDefault()
-      let response = await fetch('https://url-shortener-yh27.onrender.com', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          long_url: this.$refs.initialUrlInput.value,
-        }),
-      })
-      //If there is a bad response, console.log the response
-      if (!response.ok) {
-        console.log(response)
-        return
+  methods: {
+    async submitForm() {
+      this.isLoading = true // Show loading animation before fetch
+      try {
+        let response = await fetch('https://url-shortener-yh27.onrender.com', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            long_url: this.$refs.initialUrlInput.value,
+          }),
+        })
+        if (!response.ok) {
+          console.log(response)
+          return
+        }
+        let data = await response.json()
+        console.log(data)
+        this.shortLink = data.data.short_url
+      } catch (error) {
+        console.error('Error fetching shortened URL:', error)
+      } finally {
+        this.isLoading = false // Hide loading animation after fetch completes
       }
-      let data = await response.json()
-      console.log(data)
-      this.shortLink = data.data.short_url // shortLink as a variable to the actual short URL
+    },
+  },
+  mounted() {
+    let form = this.$refs.urlShortenerForm
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      this.submitForm() // Call the new submitForm method
     })
   },
 }
@@ -69,7 +84,8 @@ input {
   border: 1px solid #333;
   padding-left: 1em;
 }
-input::placeholder{
+
+input::placeholder {
   font-size: 1.3em;
 }
 
@@ -87,12 +103,43 @@ button:hover {
   transform: scale(1.05);
 }
 
-@media(max-width:1000px){
+.loading-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3); /* Optional: Semi-transparent overlay */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.loading-icon {
+  /* Add styles for your chosen loading animation here */
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 5px solid #fff;
+  border-top-color: transparent; /* Simulates spinning animation */
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+@media (max-width: 1000px) {
   form {
     width: 100vw;
   }
-  input::placeholder{
-  font-size: 1em;
-}
+  input::placeholder {
+    font-size: 1em;
+  }
 }
 </style>
